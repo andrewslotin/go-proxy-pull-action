84238ada58933fcda36aadfd4ede33cdc7c62de6
Derek Smith <dsmith@goodwaygroup.com>
2021-03-03 10:50:53 -0600

    feat(major versions): support major versions greater than 1

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,12 @@ if [[ "$VERSION" != "$TAG" ]]; then
   PACKAGE=github.com/${GITHUB_REPOSITORY}/${TAG%"/$VERSION"}
 fi
 
+# if version > 1, then add the version scope
+readonly MAJOR_VERSION="$(printf '%s' "$VERSION" | cut -d '.' -f 1 | sed 's/v//g')"
+if [ $((10#${MAJOR_VERSION})) -gt 1 ]; then
+  PACKAGE="$PACKAGE/v$MAJOR_VERSION"
+fi
+
 export GO111MODULE=on
 export GOPROXY="$INPUT_GOPROXY"
 
